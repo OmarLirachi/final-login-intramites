030b63f3701b14a35d38a408c4f766a1bccbacbd
OmarLirachi <612110332@utsalamanca.edu.mx>
2023-04-12 23:03:10 -0600

    primer commit

---
+++ src/views/Home.vue
@@ -0,0 +1,206 @@
+<template>
+  <v-container>
+      <v-row align="center" justify="center" >
+          <v-col cols="12" sm="10">
+            <v-card class="elevation-6 mt-10"  >
+             <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="6">
+                      <v-card-text class="mt-12">
+                        <h4
+                          class="text-center"
+                        >Inicia Sesión con tu cuenta</h4>
+                        <h6
+                          class="text-center  grey--text "
+                        >Inicia sesión con Intramites para continuar</h6>
+                        <v-row align="center" justify="center">
+                          <v-col cols="12" sm="8">
+                           
+                          <v-text-field
+                            label="Correo"
+                            outlined
+                            dense
+                            color="orange"
+                            autocomplete="false"
+                           class="mt-16"
+                          />
+                          <v-text-field
+                            label="Contraseña"
+                            outlined
+                            dense
+                            color="orange"
+                          autocomplete="false"
+                           type="password"
+                          
+                          />
+                            <v-row>
+                              <v-col cols="12" sm="7">
+                                <v-checkbox
+                       
+                        label="Recordarme"
+                        class="mt-n1"
+                        color="blue"
+                      > </v-checkbox>
+                              </v-col>
+                              <v-col cols="12" sm="5">
+                                <span class="caption deep-orange--text">Olvide mi Contraseña</span>
+                              </v-col>
+                            </v-row>
+                          <v-btn color="orange" dark block tile>Iniciar Sesión</v-btn>
+                         <h5
+                          class="text-center  grey--text mt-4 mb-3"
+                        >O Inicia con</h5>
+                         <div class="d-flex  justify-space-between align-center mx-10 mb-16">
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="red">fab fa-google</v-icon>
+                        </v-btn>
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="blue">fab fa-facebook-f</v-icon>
+                        </v-btn>
+                        
+                         </div>
+                          </v-col>
+                        </v-row>  
+                      </v-card-text>
+                    </v-col>
+                    <v-col cols="12" md="6" class="orange rounded-bl-xl" >
+                    <div style="  text-align: center; padding: 180px 0;">
+                      <v-card-text class="white--text" >
+                        <h3 class="text-center ">No tienes una cuenta creada?</h3>
+                        <h6
+                          class="text-center"
+                        >Comienza registrandote para usar Intramites</h6>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn tile outlined dark @click="step++">REGISTRARSE</v-btn>
+                      </div>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row >
+                    <v-col cols="12" md="6" class="orange rounded-br-xl">
+                     <div style="  text-align: center; padding: 180px 0;">
+                      <v-card-text class="white--text" >
+                        <h3 class="text-center ">Ya estás registrado?</h3>
+                        <h6
+                          class="text-center"
+                        >Inicia sesión para seguir</h6>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn tile outlined dark @click="step--">Inicia Sesión</v-btn>
+                      </div>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="6">
+                      <v-card-text class="mt-12">
+                        <h4
+                          class="text-center"
+                        >Registrate con Intramites</h4>
+                        <h6
+                          class="text-center  grey--text "
+                        >Ingresa tus datos para crear una cuenta</h6>
+                        <v-row align="center" justify="center">
+                          <v-col cols="12" sm="8">
+                           <v-row>
+                           <v-col cols="12" sm="6">
+                            <v-text-field
+                            label="Nombre"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                           class="mt-4"
+                          />
+                           </v-col>
+                           <v-col cols="12" sm="6">
+                            <v-text-field
+                            label="Apellido"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                           class="mt-4"
+                          />
+                           </v-col>
+                           </v-row>
+                          <v-text-field
+                            label="Correo"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                          />
+                          <v-text-field
+                            label="Contraseña"
+                            outlined
+                            dense
+                            color="blue"
+                          autocomplete="false"
+                           type="password"
+                          
+                          />
+                            <v-row>
+                              <v-col cols="12" sm="7">
+                                <v-checkbox
+                       
+                        label="Acepto T&C"
+                        class="mt-n1"
+                        color="blue"
+                      > </v-checkbox>
+                              </v-col>
+                              <v-col cols="12" sm="5">
+                                <span class="caption deep-orange--text ml-n4">Terminos & Condiciones</span>
+                              </v-col>
+                            </v-row>
+                          <v-btn color="orange" dark block tile>Registrarse</v-btn>
+                     
+                         <h5
+                          class="text-center  grey--text mt-4 mb-3"
+                        >O Registrate con</h5>
+                         <div class="d-flex  justify-space-between align-center mx-10 mb-11">
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="red">fab fa-google</v-icon>
+                        </v-btn>
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="blue">fab fa-facebook-f</v-icon>
+                        </v-btn>
+                         </div>
+                          </v-col>
+                        </v-row>  
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+  
+
+  export default {
+   data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
+  } 
+
+    
+  }
+</script>
+<style scoped>
+.v-application .rounded-bl-xl {
+    border-bottom-left-radius: 300px !important;
+}
+.v-application .rounded-br-xl {
+    border-bottom-right-radius: 300px !important;
+}
+</style>
